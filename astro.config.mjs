fa2f99bfbe408f58b742f1aaf399cd9d21189694
Gareth Foote <github@gfoote.xyz>
2025-11-05 15:54:27 +0000

    Prepping for deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,16 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import decapCmsOauth from 'astro-decap-cms-oauth';
+
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [decapCmsOauth()],
+  adapter: netlify()
 });
